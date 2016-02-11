ab00ebdb114513eb3c77b6b45c5adf7848b1fc87
Kamil Trzcinski <ayufan@ayufan.eu>
2016-02-11 18:50:02 +0100

    Test logging

---
+++ logging_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func testLogWithStatus(ww http.ResponseWriter, r *http.Request) {
+	w := newLoggingResponseWriter(ww)
+	defer w.Log(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(&w, "with-status")
+}
+
+func testLogWithoutStatus(ww http.ResponseWriter, r *http.Request) {
+	w := newLoggingResponseWriter(ww)
+	defer w.Log(r)
+	fmt.Fprint(&w, "no-status")
+}
+
+func testLogWithDoubleStatus(ww http.ResponseWriter, r *http.Request) {
+	w := newLoggingResponseWriter(ww)
+	defer w.Log(r)
+	w.WriteHeader(http.StatusOK)
+	http.Redirect(&w, r, "/test", 301)
+}
+
+func TestLoggingWriter(t *testing.T) {
+	assert.HTTPBodyContains(t, testLogWithStatus, "GET", "/test", nil, "with-status")
+	assert.HTTPBodyContains(t, testLogWithoutStatus, "GET", "/test", nil, "no-status")
+	assert.HTTPSuccess(t, testLogWithDoubleStatus, "GET", "/test", nil)
+}
