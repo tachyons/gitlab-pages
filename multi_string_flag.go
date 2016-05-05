33fca0976f95eac7ddce424080db9e381ec7388e
Nick Thomas <nick@bytemark.co.uk>
2016-05-05 13:16:32 +0100

    Allow -listen-http, -listen-https and -listen-proxy to be given more than once
    Per issue #13, sometimes you want to listen on more than one port for each type
    of listener. This commit adds support for that.

---
+++ multi_string_flag.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"strings"
+)
+
+// MultiStringFlag implements the flag.Value interface and allows a string flag
+// to be specified multiple times on the command line.
+//
+// e.g.: -listen-http 127.0.0.1:80 -listen-http [::1]:80
+type MultiStringFlag []string
+
+// String returns the list of parameters joined with a commas (",")
+func (s *MultiStringFlag) String() string {
+	return strings.Join(*s, ",")
+}
+
+// Set appends the value to the list of parameters
+func (s *MultiStringFlag) Set(value string) error {
+	*s = append(*s, value)
+	return nil
+}
