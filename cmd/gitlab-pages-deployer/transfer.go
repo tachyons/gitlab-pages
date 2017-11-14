ffa7957c9990bdc84d85d2a244fc695bb1c1a59e
Kamil Trzcinski <ayufan@ayufan.eu>
2017-11-14 21:00:44 +0100

    Force to reload daemon on project transfer

--- cmd/gitlab-pages-deployer/transfer.go
+++ cmd/gitlab-pages-deployer/transfer.go
@@ -16,11 +16,15 @@ func panicOnFileSystemError(err error) {
 func renameNamespaceJob(message *workers.Msg, projectID int64, fullPathWas, fullPath string) {
 	err := os.Rename(filepath.Join(*deployerRoot, fullPathWas), filepath.Join(*deployerRoot, fullPath))
 	panicOnFileSystemError(err)
+
+	touchDaemon()
 }
 
 func renameProjectJob(message *workers.Msg, projectID int64, pathWas, path, fullPath string) {
 	err := os.Rename(filepath.Join(*deployerRoot, fullPath, pathWas), filepath.Join(*deployerRoot, fullPath, path))
 	panicOnFileSystemError(err)
+
+	touchDaemon()
 }
 
 func moveProjectJob(message *workers.Msg, projectID int64, path, fullPathWas, fullPath string) {
@@ -29,9 +33,13 @@ func moveProjectJob(message *workers.Msg, projectID int64, path, fullPathWas, fu
 
 	err = os.Rename(filepath.Join(*deployerRoot, fullPathWas, path), filepath.Join(*deployerRoot, fullPath, path))
 	panicOnFileSystemError(err)
+
+	touchDaemon()
 }
 
 func removeJob(message *workers.Msg, projectID int64, namespacePath, path string) {
 	err := os.RemoveAll(filepath.Join(*deployerRoot, namespacePath, path))
 	panicOnFileSystemError(err)
+
+	touchDaemon()
 }
