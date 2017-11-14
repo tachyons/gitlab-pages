43895d6497eb49c5f7c206f01786e42acf56b48a
Kamil Trzcinski <ayufan@ayufan.eu>
2017-11-14 20:45:09 +0100

    Implement transfer methods

--- cmd/gitlab-pages-deployer/transfer.go
+++ cmd/gitlab-pages-deployer/transfer.go
@@ -1,19 +1,37 @@
 package main
 
-import workers "github.com/jrallison/go-workers"
+import (
+	"os"
+	"path/filepath"
 
-func renameNamespaceJob(message *workers.Msg, projectId int64, fullPathWas, fullPath string) {
+	workers "github.com/jrallison/go-workers"
+)
 
+func panicOnFileSystemError(err error) {
+	if err == nil && !os.IsNotExist(err) {
+		panic(err)
+	}
 }
 
-func renameProjectJob(message *workers.Msg, projectId int64, pathWas, path, fullPath string) {
-
+func renameNamespaceJob(message *workers.Msg, projectID int64, fullPathWas, fullPath string) {
+	err := os.Rename(filepath.Join(*deployerRoot, fullPathWas), filepath.Join(*deployerRoot, fullPath))
+	panicOnFileSystemError(err)
 }
 
-func moveProjectJob(message *workers.Msg, projectId int64, path, fullPathWas, fullPath string) {
-
+func renameProjectJob(message *workers.Msg, projectID int64, pathWas, path, fullPath string) {
+	err := os.Rename(filepath.Join(*deployerRoot, fullPath, pathWas), filepath.Join(*deployerRoot, fullPath, path))
+	panicOnFileSystemError(err)
 }
 
-func removeJob(message *workers.Msg, projectId int64, namespacePath, path string) {
+func moveProjectJob(message *workers.Msg, projectID int64, path, fullPathWas, fullPath string) {
+	err := os.MkdirAll(filepath.Join(*deployerRoot, fullPath), 0750)
+	panicOnFileSystemError(err)
 
+	err = os.Rename(filepath.Join(*deployerRoot, fullPathWas, path), filepath.Join(*deployerRoot, fullPath, path))
+	panicOnFileSystemError(err)
+}
+
+func removeJob(message *workers.Msg, projectID int64, namespacePath, path string) {
+	err := os.RemoveAll(filepath.Join(*deployerRoot, namespacePath, path))
+	panicOnFileSystemError(err)
 }
