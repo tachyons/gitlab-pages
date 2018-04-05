c356b730338d76a45c81612d5f051dcb567f61c7
Jacob Vosmaer <jacob@gitlab.com>
2018-04-05 13:43:36 +0200

    Add rpc call in test

--- internal/service/deploy/deploy.go
+++ internal/service/deploy/deploy.go
@@ -1,3 +1,14 @@
 package deploy
 
+import (
+	"context"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	pb "gitlab.com/gitlab-org/gitlab-pages-proto/go"
+)
+
 type server struct{}
+
+func (*server) DeleteSite(context.Context, *pb.DeleteSiteRequest) (*empty.Empty, error) {
+	return &empty.Empty{}, nil
+}
--- internal/service/deploy/deploy_test.go
+++ internal/service/deploy/deploy_test.go
@@ -1,17 +1,39 @@
 package deploy
 
 import (
+	"context"
 	"net"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/require"
 	pb "gitlab.com/gitlab-org/gitlab-pages-proto/go"
 	"google.golang.org/grpc"
 )
 
-func runDeployServer(t *testing.T) (*grpc.Server, string) {
+var (
+	serverSocketPath = "testdata/grpc.socket"
+)
+
+func TestDeleteSite(t *testing.T) {
+	s := runDeployServer(t)
+	defer s.Stop()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	client, conn := newDeployClient(t)
+	defer conn.Close()
+
+	req := &pb.DeleteSiteRequest{}
+
+	_, err := client.DeleteSite(ctx, req)
+	require.NoError(t, err)
+}
+
+func runDeployServer(t *testing.T) *grpc.Server {
 	grpcServer := grpc.NewServer()
 
-	serverSocketPath := "testdata/grpc.socket"
 	listener, err := net.Listen("unix", serverSocketPath)
 
 	if err != nil {
@@ -22,5 +44,20 @@ func runDeployServer(t *testing.T) (*grpc.Server, string) {
 
 	go grpcServer.Serve(listener)
 
-	return grpcServer, serverSocketPath
+	return grpcServer
+}
+
+func newDeployClient(t *testing.T) (pb.DeployServiceClient, *grpc.ClientConn) {
+	connOpts := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}),
+	}
+	conn, err := grpc.Dial(serverSocketPath, connOpts...)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return pb.NewDeployServiceClient(conn), conn
 }
