161347ef496e73ff686242d2a19725c81a844d58
Nick Thomas <nick@gitlab.com>
2018-10-24 03:52:31 +0100

    Remove a workaround for golint

--- Makefile.build.mk
+++ Makefile.build.mk
@@ -4,9 +4,7 @@ all: gitlab-pages
 
 setup: clean .GOPATH/.ok
 	go get golang.org/x/tools/cmd/goimports
-	# Workaround for broken 'go get golang.org/x/lint' due to Google hosting problems
-	git clone --quiet https://github.com/golang/lint $(GOPATH)/src/golang.org/x/lint
-	go install golang.org/x/lint/golint
+	go get golang.org/x/lint/golint
 	go get golang.org/x/tools/cmd/goimports
 	go get github.com/wadey/gocovmerge
 	go get github.com/fzipp/gocyclo
