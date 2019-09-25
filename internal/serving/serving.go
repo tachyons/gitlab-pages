508be9806c17d6ab8ca75104f831c7a7a9dc9a3a
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-09-25 12:46:42 +0200

    Remove serving package that we do not use yet

--- internal/serving/serving.go
+++
@@ -1,9 +0,0 @@
-package serving
-
-import "net/http"
-
-// Serving represents an interface used to serve pages for a given domain /
-// address
-type Serving interface {
-	ServeHTTP(http.ResponseWriter, *http.Request)
-}
