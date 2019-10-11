e02b155f4d7f1078a06970d3d3faf051bf22bf35
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-10-11 10:15:23 +0200

    Make it possible to switch source for some domains

--- internal/source/domains.go
+++ internal/source/domains.go
@@ -1,6 +1,7 @@
 package source
 
 import (
+	"gitlab.com/gitlab-org/gitlab-pages/internal/domain"
 	"gitlab.com/gitlab-org/gitlab-pages/internal/source/disk"
 )
 
@@ -17,4 +18,11 @@ func NewDomains() *Domains {
 	return &Domains{
 		Source: disk.New(),
 	}
+}
+
+// GetDomain overridden here allows us to switch behavior and the domains
+// source for some subset of domains, to test / PoC the new GitLab Domains
+// Source that we plan to introduce
+func (d *Domains) GetDomain(name string) *domain.Domain {
+	return d.Source.GetDomain(name)
 }
