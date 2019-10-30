eb17ac651ffe0a8c4f01c541c807ae793db55043
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-10-30 12:09:21 +0100

    Fix linter warning in gitlab source code

--- internal/source/gitlab/client.go
+++ internal/source/gitlab/client.go
@@ -1,6 +1,6 @@
 package gitlab
 
-// client is an internal HTTP client used for communication with GitLab
+// Client is an internal HTTP client used for communication with GitLab
 // instance
 type Client interface {
 	Resolve(domain string) *Lookup
