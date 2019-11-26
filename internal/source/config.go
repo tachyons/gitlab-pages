5681803e1073edbc4fc3fa9c8f2f48ab6199b887
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-11-26 13:20:51 +0100

    Simplify gitlab source config

--- internal/source/config.go
+++ internal/source/config.go
@@ -1,8 +1,7 @@
 package source
 
+import "gitlab.com/gitlab-org/gitlab-pages/internal/source/gitlab/client"
+
 // Config represents an interface that is configuration provider for client
 // capable of comunicating with GitLab
-type Config interface {
-	GitlabServerURL() string
-	GitlabClientSecret() []byte
-}
+type Config client.Config
