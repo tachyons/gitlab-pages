e505889d01e81427686535f18f77b00c66446961
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-12-02 11:44:18 +0100

    Test domains source not fully configured

--- internal/source/domains_test.go
+++ internal/source/domains_test.go
@@ -10,7 +10,38 @@ import (
 	"gitlab.com/gitlab-org/gitlab-pages/internal/source/disk"
 )
 
-func TestHasDomain(t *testing.T) {
+type sourceConfig struct {
+	api    string
+	secret string
+}
+
+func (c sourceConfig) GitlabServerURL() string {
+	return c.api
+}
+
+func (c sourceConfig) GitlabAPISecret() []byte {
+	return []byte(c.secret)
+}
+
+func TestDomainSources(t *testing.T) {
+	t.Run("when GitLab API URL has been provided", func(t *testing.T) {
+		domains, err := NewDomains(sourceConfig{api: "https://gitlab.com"})
+		require.Nil(t, err)
+
+		assert.NotNil(t, domains.gitlab)
+		assert.NotNil(t, domains.disk)
+	})
+
+	t.Run("when GitLab API has not been provided", func(t *testing.T) {
+		domains, err := NewDomains(sourceConfig{})
+		require.Nil(t, err)
+
+		assert.Nil(t, domains.gitlab)
+		assert.NotNil(t, domains.disk)
+	})
+}
+
+func TestGetDomain(t *testing.T) {
 	newSourceDomains = []string{"new-source-test.gitlab.io"}
 	brokenSourceDomain = "pages-broken-poc.gitlab.io"
 
@@ -59,5 +90,15 @@ func TestHasDomain(t *testing.T) {
 
 		assert.Nil(t, domain)
 		assert.EqualError(t, err, "broken test domain used")
+	})
+
+	t.Run("when requesting a test domain in case of the source not being fully configured", func(t *testing.T) {
+		domains, err := NewDomains(sourceConfig{})
+		require.NoError(t, err)
+
+		domain, err := domains.GetDomain("new-source-test.gitlab.io")
+
+		assert.Nil(t, domain)
+		assert.NoError(t, err)
 	})
 }
