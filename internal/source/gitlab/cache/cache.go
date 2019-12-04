cafc4369750e608ac4be5b61240e90265fdcb5a5
Grzegorz Bizon <grzesiek.bizon@gmail.com>
2019-12-04 14:27:06 +0100

    Add more information about how gitlab source caching works

--- internal/source/gitlab/cache/cache.go
+++ internal/source/gitlab/cache/cache.go
@@ -20,7 +20,51 @@ func NewCache(client api.Client) *Cache {
 	}
 }
 
-// Resolve is going to return a Lookup based on a domain name
+// Resolve is going to return a Lookup based on a domain name. The caching
+// algorithm works as follows:
+// - We first check if the cache entry exists, and if it is up-to-date. If it
+//   is fresh we return the &Lookup entry from cache and it is a cache hit.
+// - If entry is not up-to-date, we schedule an asynchronous retrieval of the
+//   latest configuration we are going to obtain through the API, and we
+//   immediately return an old value, to avoid blocking clients. In this case
+//   it is also a cache hit.
+// - If cache entry has not been populated with a Lookup information yet, we
+//   block all the clients and make them wait until we retrieve the Lookup from
+//   the GitLab API.
+//
+// We are going to retrieve a Lookup from GitLab API using Retriever type. In
+// case of failures (when GitLab API client returns an error) we will retry the
+// operation a few times. In case of an erroneous response, we will cache it,
+// and it get recycled as every other cache entry.
+//
+// Examples:
+// 1. Everything works
+//  - a client opens pages
+//  - we create a new cache entry
+//  - cache entry needs warm up
+//  - a client waits until we retrieve a lookup
+//  - we successfuly retrieve a lookup
+//  - we cache this response
+//  - and we pass it upstream to all clients
+// 2. A domain does not exist
+//  - a client opens pages
+//  - we create a new cache entry
+//  - cache entry needs warm up
+//  - a client waits until we retrieve a lookup
+//  - GitLab responded with a lookup that contains information about domain
+//    not being found
+//  - we cache this response
+//  - we pass this lookup upstream to all theclient
+// 3. GitLab is not responding
+//  - a client opens pages
+//  - we create a new cache entry
+//  - cache entry needs warm up
+//  - a client waits until we retrieve a lookup
+//  - GitLab does not respond or responds with an error
+//  - we retry this information a few times
+//  - we create a lookup that contains information about an error
+//  - we cache this response
+//  - we pass this lookup upstream to all the clients
 func (c *Cache) Resolve(ctx context.Context, domain string) *api.Lookup {
 	entry := c.store.LoadOrCreate(domain)
 
