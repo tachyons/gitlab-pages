de199e239841b1dd771d0ff736e70094fa50ecc0
Hossein Pursultani <hpursultani@gitlab.com>
2019-12-11 06:54:52 +1100

    Add documentation for offline UBI build
    This commit also includes improvements to offline build scripts to
    allow them to be executed from any location.

--- build-scripts/ubi-offline/build.sh
+++ build-scripts/ubi-offline/build.sh
@@ -1,36 +1,51 @@
 #!/bin/bash
 
-# NOTICE: This script requires `docker`.
+#
+# Builds all UBI-based images in the right order.
+#
+# USAGE: 
+#
+#   build.sh
+#
+# NOTE: 
+#
+#   This script requires `docker`.
+#
 
 set -euxo pipefail
 
+SCRIPT_HOME="$( cd "${BASH_SOURCE[0]%/*}" > /dev/null 2>&1 && pwd )"
+
 TAG=${1:-latest}
 REPOSITORY=${2:-}
+WORKSPACE="${SCRIPT_HOME}/build"
+DOCKERFILE_EXT='.ubi8'
+TAG_EXT='-ubi8'
 
-DOCKERFILE_EXT=".ubi8"
-TAG_EXT="-ubi8"
+mkdir -p "${WORKSPACE}"
 
-imageName() {
+qualifiedName() {
   printf "${REPOSITORY}${1}:${TAG}${TAG_EXT}"
 }
 
 buildImage() {
-  IMAGE="${1}"
-  CONTEXT="${IMAGE%*-ee}"
+  IMAGE_NAME="${1}"
+  IMAGE_DIR="${IMAGE_NAME%*-ee}"
+  CONTEXT="${SCRIPT_HOME}/../../${IMAGE_DIR}"
   { 
     docker build \
       -f "${CONTEXT}/Dockerfile${DOCKERFILE_EXT}" \
-      -t "$(imageName ${IMAGE})" \
+      -t "$(qualifiedName ${IMAGE_NAME})" \
       ${DOCKER_OPTS:-} \
-      "${CONTEXT}" | tee ${CONTEXT}.out 
+      "${CONTEXT}" 2>&1 | tee "${WORKSPACE}/${IMAGE_DIR}.out"
   } || {
-    echo "${CONTEXT}" >> failed.log
+    echo "${IMAGE_DIR}" >> "${WORKSPACE}/failed.log"
   }
 }
 
 # Cleanup log outputs from previous build
 
-rm -f *.out failed.log
+rm -f "${WORKSPACE}"/*.out "${WORKSPACE}/failed.log"
 
 # Stage one
 
@@ -43,7 +58,7 @@ wait
 
 # Stage two
 
-DOCKER_OPTS="--build-arg RUBY_IMAGE=$(imageName gitlab-ruby)"
+DOCKER_OPTS="--build-arg RUBY_IMAGE=$(qualifiedName gitlab-ruby)"
 buildImage git-base &
 buildImage gitlab-exporter &
 buildImage gitlab-mailroom &
@@ -55,10 +70,10 @@ wait
 
 # Stage three
 
-DOCKER_OPTS="--build-arg GIT_IMAGE=$(imageName git-base)"
+DOCKER_OPTS="--build-arg GIT_IMAGE=$(qualifiedName git-base)"
 buildImage gitaly &
 
-DOCKER_OPTS="--build-arg RAILS_IMAGE=$(imageName gitlab-rails-ee)"
+DOCKER_OPTS="--build-arg RAILS_IMAGE=$(qualifiedName gitlab-rails-ee)"
 buildImage gitlab-geo-logcursor &
 buildImage gitlab-sidekiq-ee &
 buildImage gitlab-task-runner-ee &
