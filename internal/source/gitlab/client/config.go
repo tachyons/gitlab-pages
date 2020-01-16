9e8252199cfe9968c23008b8154b394f03f025e1
Jaime Martinez <jmartinez@gitlab.com>
2020-01-16 15:08:25 +1100

    Add http client connection timeout and JWT token expiry as config parameters

--- internal/source/gitlab/client/config.go
+++ internal/source/gitlab/client/config.go
@@ -1,8 +1,12 @@
 package client
 
+import "time"
+
 // Config represents an interface that is configuration provider for client
 // capable of comunicating with GitLab
 type Config interface {
 	GitlabServerURL() string
 	GitlabAPISecret() []byte
+	GitlabClientConnectionTimeout() time.Duration
+	GitlabJWTTokenExpiry() time.Duration
 }
