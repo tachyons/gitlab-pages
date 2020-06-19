ac16c278cfa0cc096fa5aaa308cfe355b6bc88c1
Jason Plum <jplum@gitlab.com>
2020-06-19 14:47:48 -0400

    gitlab-rails: rubocop'd new dependencies checks
    Alterations based on Rubocop suggestions.

--- gitlab-rails/scripts/lib/checks/postgresql.rb
+++ gitlab-rails/scripts/lib/checks/postgresql.rb
@@ -1,66 +1,77 @@
+# frozen_string_literal: true
+
 require 'yaml'
 require 'active_record'
 
-class Checks
-    class PostgreSQL
-        @@config = nil
-        @@database_version = 0
-        
-        def self.wait_for_timeout()
-            ENV["WAIT_FOR_TIMEOUT"].to_i
-        end
+module Checks
+  # Perform checks of PostgreSQL dependency.
+  # Usage: `Checks::PostgreSQL.run`
+  module PostgreSQL
+    @@config = nil
+    @@database_version = 0
 
-        def self.sleep_duration()
-            ENV["SLEEP_DURATION"].to_i
-        end
-
-        def self.codebase_schema_version()
-            ENV["SCHEMA_VERSION"].to_i
-        end
-
-        def self.config()
-            return @@config if @@config
-            config = YAML.load_file("#{ENV["CONFIG_DIRECTORY"]}/#{ENV["DATABASE_FILE"]}")
-            @@config = config['production']
-        end
-
-        def self.database_schema_version()
-            # load ActiveRecord with configuration
-            ActiveRecord::Base.establish_connection(config)
-            # Attempt to fetch the current migrations version
-            begin
-                @@database_version = ActiveRecord::Migrator.current_version
-                true
-            rescue PG::ConnectionBad => e
-                puts "PostgreSQL Error: #{e.message}"
-                false
-            rescue RuntimeError => e
-                puts "Error: #{e.message}"
-                false
-            end
-        end
-
-        def self.check_schema_version()
-            success = database_schema_version
-
-            puts "Database Schema - current: #{@@database_version}, codebase: #{codebase_schema_version}"
-            
-            puts "NOTICE: Database has not been initialized yet." unless @@database_version.to_i > 0
-
-            return true unless ENV["BYPASS_SCHEMA_VERSION"].nil? and success
-
-            return (@@database_version <= codebase_schema_version)
-        end
-
-        def self.run()
-            counter = 1
-            passed = false
-            until counter == wait_for_timeout or passed do
-                passed = check_schema_version
-                sleep sleep_duration unless passed
-                counter+=1
-            end
-            passed
-        end
+    def self.run
+      counter = 1
+      passed = false
+      until (counter == wait_for_timeout) || passed
+        passed = check_schema_version
+        sleep sleep_duration unless passed
+        counter += 1
+      end
+      passed
     end
+
+    def self.wait_for_timeout
+      ENV['WAIT_FOR_TIMEOUT'].to_i
+    end
+
+    def self.sleep_duration
+      ENV['SLEEP_DURATION'].to_i
+    end
+
+    def self.codebase_schema_version
+      ENV['SCHEMA_VERSION'].to_i
+    end
+
+    def self.config_directory
+      ENV['CONFIG_DIRECTORY']
+    end
+
+    def self.database_file
+      ENV['DATABASE_FILE']
+    end
+
+    def self.config
+      return @@config if @@config
+
+      config = YAML.load_file(File.join(config_directory, database_file))
+      @@config = config['production']
+    end
+
+    def self.database_schema_version
+      ActiveRecord::Base.establish_connection(config)
+      begin
+        @@database_version = ActiveRecord::Migrator.current_version
+        true
+      rescue PG::ConnectionBad => e
+        puts "PostgreSQL Error: #{e.message}"
+        false
+      rescue RuntimeError => e
+        puts "Error: #{e.message}"
+        false
+      end
+    end
+
+    def self.check_schema_version
+      success = database_schema_version
+
+      puts "Database Schema - current: #{@@database_version}, codebase: #{codebase_schema_version}"
+
+      puts 'NOTICE: Database has not been initialized yet.' unless @@database_version.to_i.positive?
+
+      return true unless ENV['BYPASS_SCHEMA_VERSION'].nil? && success
+
+      (@@database_version <= codebase_schema_version)
+    end
+  end
 end
--- gitlab-rails/scripts/lib/checks/redis.rb
+++ gitlab-rails/scripts/lib/checks/redis.rb
@@ -1,65 +1,66 @@
+# frozen_string_literal: true
+
 require 'yaml'
 require 'redis'
 # Get Hash.deep_symbolize_keys from Rails
 # - allows transparent transformation of Sentinel configuration
 require 'active_support/core_ext/hash/keys'
 
-
-class Checks
-    class Redis
-        def self.wait_for_timeout()
-            ENV["WAIT_FOR_TIMEOUT"].to_i
-        end
-
-        def self.sleep_duration()
-            ENV["SLEEP_DURATION"].to_i
-        end
-
-        def self.ping_redis(file)
-            begin
-                resque_yaml = YAML.load_file(file)
-                config = resque_yaml['production'].deep_symbolize_keys
-                # configure the Redis client
-                redis = ::Redis.new( config )
-                url = "#{redis._client.scheme}://#{redis._client.host}:#{redis._client.port}"
-                puts "> Connecting to '#{url}' from #{file}"
-                # "ping" the server
-                redis.ping
-            rescue RuntimeError => e
-                puts "- FAILED connecting to '#{url}' from #{file}, through #{redis._client.host}"
-                puts "  ERROR: #{e.message}"
-                false
-            else
-                # nicely disconnect
-                redis.disconnect!
-                puts "+ SUCCESS connecting to '#{url}' from #{file}, through #{redis._client.host}"
-                true
-            end
-        end
-
-        def self.check_redis_connectivity()
-            Dir.chdir(ENV["CONFIG_DIRECTORY"])
-            files = Dir.glob(["resque.yml", "redis\..*.yml", "cable.yml"])
-            puts "Checking: #{files.join(", ")}"
-            results = files.map do |resque_file|
-                Thread.new { Redis.ping_redis(resque_file) }
-            end.map(&:value)
-
-            # Collect the checks that passed.
-            checks_passed = results.select {|r| r }
-            # Return pass/fail based on all checks passing
-            checks_passed.count == files.count
-        end
-
-        def self.run()
-            counter = 1
-            passed = false
-            until counter == wait_for_timeout or passed do
-                passed = check_redis_connectivity
-                sleep sleep_duration unless passed
-                counter+=1
-            end
-            passed
-        end
+module Checks
+  # Perform checks of Redis dependency.
+  # Usage: `Checks::Redis.run`
+  module Redis
+    def self.run
+      counter = 1
+      passed = false
+      until (counter == wait_for_timeout) || passed
+        passed = check_redis_connectivity
+        sleep sleep_duration unless passed
+        counter += 1
+      end
+      passed
     end
+
+    def self.wait_for_timeout
+      ENV['WAIT_FOR_TIMEOUT'].to_i
+    end
+
+    def self.sleep_duration
+      ENV['SLEEP_DURATION'].to_i
+    end
+
+    def self.redis_config(file)
+      resque_yaml = YAML.load_file(file)
+      resque_yaml['production'].deep_symbolize_keys
+    end
+
+    def self.ping_redis(file)
+      redis = ::Redis.new(redis_config(file))
+      url = "#{redis._client.scheme}://#{redis._client.host}:#{redis._client.port}"
+      begin
+        redis.ping
+      rescue RuntimeError => e
+        puts "- FAILED connecting to '#{url}' from #{file}, through #{redis._client.host}\r\nERROR: #{e.message}"
+        false
+      else
+        redis.disconnect!
+        puts "+ SUCCESS connecting to '#{url}' from #{file}, through #{redis._client.host}"
+        true
+      end
+    end
+
+    def self.check_redis_connectivity
+      Dir.chdir(ENV['CONFIG_DIRECTORY'])
+      files = Dir.glob(['resque.yml', "redis\..*.yml", 'cable.yml'])
+      puts "Checking: #{files.join(', ')}"
+      results = files.map do |resque_file|
+        Thread.new { Redis.ping_redis(resque_file) }
+      end.map(&:value)
+
+      # Collect the checks that passed.
+      checks_passed = results.select { |r| r }
+      # Return pass/fail based on all checks passing
+      checks_passed.count == files.count
+    end
+  end
 end
