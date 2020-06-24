07a31a840fa0212418c23d9b6853c2c877422a96
DJ Mountney <dj@gitlab.com>
2020-06-24 15:18:55 -0700

    Add the sha256 sum to the assets, so we don't need to caluclate it later

--- build-scripts/ubi-offline/assets-release.sh
+++ build-scripts/ubi-offline/assets-release.sh
@@ -4,18 +4,20 @@ set -e
 
 cd artifacts/
 
-ASSETS_PACK="ubi8-build-dependencies-${UBI_RELEASE_TAG}.tar"
+ASSETS_DIR="ubi8-build-dependencies-${UBI_RELEASE_TAG}"
+ASSETS_PACK="${ASSETS_DIR}.tar"
 ASSETS_URL="http://${UBI_ASSETS_AWS_BUCKET}.s3.amazonaws.com/${ASSETS_PACK}"
 gpg --batch --quiet --yes --armor --export --output gpg
 tar -cvf ${ASSETS_PACK} -C ubi .
-rm -r ubi
 gpg --passphrase "${GPG_KEY_PASSPHRASE}" --batch --quiet --yes --armor --pinentry-mode loopback --detach-sign ${ASSETS_PACK}
+sha256sum ${ASSETS_PACK} | awk '{print $1}' > "${ASSETS_PACK}.sha256"
 aws s3 cp --quiet --acl public-read --content-type application/x-pem-file gpg "s3://${UBI_ASSETS_AWS_BUCKET}/gpg"
 aws s3 cp --quiet --acl public-read --content-type application/x-pem-file ${ASSETS_PACK}.asc "s3://${UBI_ASSETS_AWS_BUCKET}/${ASSETS_PACK}.asc"
+aws s3 cp --quiet --acl public-read --content-type application/x-pem-file ${ASSETS_PACK}.sha256 "s3://${UBI_ASSETS_AWS_BUCKET}/${ASSETS_PACK}.sha256"
 aws s3 cp --quiet --acl public-read --content-type application/x-tar ${ASSETS_PACK} "s3://${UBI_ASSETS_AWS_BUCKET}/${ASSETS_PACK}"
 curl --retry 6 -f -H "PRIVATE-TOKEN:${UBI_RELEASE_PAT}" -H 'Content-Type:application/json' --data \
     "$(printf \
-      '{"tag_name":"%s","ref":"%s","name":"%s","description":"%s","assets":{"links":[{"name":"%s","url":"%s"},{"name":"%s","url":"%s"}]}}' \
+      '{"tag_name":"%s","ref":"%s","name":"%s","description":"%s","assets":{"links":[{"name":"%s","url":"%s"},{"name":"%s","url":"%s"},{"name":"%s","url":"%s"}]}}' \
       "${UBI_RELEASE_TAG}" \
       "${UBI_RELEASE_TAG}" \
       "Release ${UBI_RELEASE_TAG}" \
@@ -24,5 +26,10 @@ curl --retry 6 -f -H "PRIVATE-TOKEN:${UBI_RELEASE_PAT}" -H 'Content-Type:applica
       "${ASSETS_URL}" \
       "${ASSETS_PACK}.asc" \
       "${ASSETS_URL}.asc" \
+      "${ASSETS_PACK}.sha256" \
+      "${ASSETS_URL}.sha256" \
     )" \
     "${RELEASE_API}"
+
+# Exclude duplicates from the cache
+rm -r ubi
