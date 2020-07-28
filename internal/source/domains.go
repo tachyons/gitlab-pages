7c9cc5a4f19f1f160f49f0a8f227cfa7af8be181
Jaime Martinez <jmartinez@gitlab.com>
2020-07-28 14:39:12 +1000

    Set configSource to the Domains struct

--- internal/source/domains.go
+++ internal/source/domains.go
@@ -1,40 +1,36 @@
 package source
 
 import (
-	"errors"
+	"fmt"
 	"regexp"
-	"time"
-
-	log "github.com/sirupsen/logrus"
 
 	"gitlab.com/gitlab-org/gitlab-pages/internal/domain"
-	"gitlab.com/gitlab-org/gitlab-pages/internal/rollout"
 	"gitlab.com/gitlab-org/gitlab-pages/internal/source/disk"
-	"gitlab.com/gitlab-org/gitlab-pages/internal/source/domains/gitlabsourceconfig"
 	"gitlab.com/gitlab-org/gitlab-pages/internal/source/gitlab"
 )
 
 var (
-	gitlabSourceConfig gitlabsourceconfig.GitlabSourceConfig
-
 	// serverlessDomainRegex is a regular expression we use to check if a domain
 	// is a serverless domain, to short circuit gitlab source rollout. It can be
 	// removed after the rollout is done
 	serverlessDomainRegex = regexp.MustCompile(`^[^.]+-[[:xdigit:]]{2}a1[[:xdigit:]]{10}f2[[:xdigit:]]{2}[[:xdigit:]]+-?.*`)
 )
 
-func init() {
-	// Start watching the config file for domains that will use the new `gitlab` source,
-	// to be removed once we switch completely to using it.
-	go gitlabsourceconfig.WatchForGitlabSourceConfigChange(&gitlabSourceConfig, 1*time.Minute)
-}
+type configSource int
+
+const (
+	sourceGitlab configSource = iota
+	sourceDisk
+	sourceAuto
+)
 
 // Domains struct represents a map of all domains supported by pages. It is
 // currently using two sources during the transition to the new GitLab domains
 // source.
 type Domains struct {
-	gitlab Source
-	disk   *disk.Disk // legacy disk source
+	configSource configSource
+	gitlab       Source
+	disk         *disk.Disk // legacy disk source
 }
 
 // NewDomains is a factory method for domains initializing a mutex. It should
@@ -42,34 +38,46 @@ type Domains struct {
 // nil value.
 func NewDomains(config Config) (*Domains, error) {
 	domains := &Domains{
+		// TODO: disable domains.disk https://gitlab.com/gitlab-org/gitlab-pages/-/issues/382
 		disk: disk.New(),
 	}
 
+	domains.setConfigSource(config)
+
 	// Creating a glClient will start polling connectivity in the background
-	glClient, glErr := newGitlabClient(config)
-
-	// TODO: handle DomainConfigSource == "gitlab" || "auto" https://gitlab.com/gitlab-org/gitlab/-/issues/218358
-	switch config.DomainConfigSource() {
-	case "disk":
-		return domains, nil
+	glClient, err := newGitlabClient(config)
+	if err != nil && domains.configSource != sourceDisk {
+		return nil, err
 	}
 
-	// return glErr when no domain config source is specified
-	if glErr != nil {
-		log.WithError(glErr).Warn("failed to create GitLab domains source")
-		return nil, glErr
-	}
-
-	if glClient != nil {
+	// TODO: handle DomainConfigSource == "auto" https://gitlab.com/gitlab-org/gitlab/-/issues/218358
+	// attach gitlab by default when source is not disk (auto, gitlab)
+	if domains.configSource != sourceDisk {
 		domains.gitlab = glClient
 	}
 
 	return domains, nil
 }
 
+// defaults to disk
+func (d *Domains) setConfigSource(config Config) {
+	switch config.DomainConfigSource() {
+	case "gitlab":
+		// TODO:  https://gitlab.com/gitlab-org/gitlab/-/issues/218357
+		d.configSource = sourceGitlab
+	case "auto":
+		// TODO: https://gitlab.com/gitlab-org/gitlab/-/issues/218358
+		d.configSource = sourceAuto
+	case "disk":
+		fallthrough
+	default:
+		d.configSource = sourceDisk
+	}
+}
+
 func newGitlabClient(config Config) (*gitlab.Gitlab, error) {
 	if len(config.InternalGitLabServerURL()) == 0 || len(config.GitlabAPISecret()) == 0 {
-		return nil, nil
+		return nil, fmt.Errorf("missing -internal-gitlab-server and/or -api-secret-key")
 	}
 
 	return gitlab.New(config)
@@ -80,10 +88,6 @@ func newGitlabClient(config Config) (*gitlab.Gitlab, error) {
 // for some subset of domains, to test / PoC the new GitLab Domains Source that
 // we plan to use to replace the disk source.
 func (d *Domains) GetDomain(name string) (*domain.Domain, error) {
-	if name == gitlabSourceConfig.Domains.Broken {
-		return nil, errors.New("broken test domain used")
-	}
-
 	return d.source(name).GetDomain(name)
 }
 
@@ -101,7 +105,7 @@ func (d *Domains) IsReady() bool {
 }
 
 func (d *Domains) source(domain string) Source {
-	if d.gitlab == nil || !d.gitlab.IsReady() {
+	if d.gitlab == nil {
 		return d.disk
 	}
 
@@ -112,21 +116,11 @@ func (d *Domains) source(domain string) Source {
 		return d.gitlab
 	}
 
-	for _, name := range gitlabSourceConfig.Domains.Enabled {
-		if domain == name {
-			return d.gitlab
-		}
-	}
-
-	r := gitlabSourceConfig.Domains.Rollout
-
-	enabled, err := rollout.Rollout(domain, r.Percentage, r.Stickiness)
-	if err != nil {
-		log.WithError(err).Error("Rollout error")
+	if d.configSource == sourceDisk {
 		return d.disk
 	}
 
-	if enabled {
+	if d.gitlab.IsReady() {
 		return d.gitlab
 	}
 
--- internal/source/domains_test.go
+++ internal/source/domains_test.go
@@ -1,7 +1,6 @@
 package source
 
 import (
-	"math/rand"
 	"testing"
 	"time"
 
@@ -37,39 +36,59 @@ func (c sourceConfig) DomainConfigSource() string {
 }
 
 func TestDomainSources(t *testing.T) {
-	t.Run("when GitLab API URL has been provided", func(t *testing.T) {
-		domains, err := NewDomains(sourceConfig{api: "https://gitlab.com", secret: "abc"})
-		require.NoError(t, err)
+	tests := []struct {
+		name            string
+		sourceConfig    sourceConfig
+		wantErr         bool
+		expectGitlabNil bool
+	}{
+		{
+			name:            "no_source_config",
+			sourceConfig:    sourceConfig{},
+			wantErr:         false,
+			expectGitlabNil: true,
+		},
+		{
+			name:            "disk_source",
+			sourceConfig:    sourceConfig{domainSource: "disk"},
+			wantErr:         false,
+			expectGitlabNil: true,
+		},
+		{
+			name:         "gitlab_source_success",
+			sourceConfig: sourceConfig{api: "https://gitlab.com", secret: "abc", domainSource: "gitlab"},
+			wantErr:      false,
+		},
+		{
+			name:         "gitlab_source_no_url",
+			sourceConfig: sourceConfig{api: "", secret: "abc", domainSource: "gitlab"},
+			wantErr:      true,
+		},
+		{
+			name:         "gitlab_source_no_secret",
+			sourceConfig: sourceConfig{api: "https://gitlab.com", secret: "", domainSource: "gitlab"},
+			wantErr:      true,
+		},
+	}
 
-		require.NotNil(t, domains.gitlab)
-		require.NotNil(t, domains.disk)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			domains, err := NewDomains(tt.sourceConfig)
+			if tt.wantErr {
+				require.EqualError(t, err, "missing -internal-gitlab-server and/or -api-secret-key")
+				return
+			}
+			require.NoError(t, err)
 
-	t.Run("when GitLab API has not been provided", func(t *testing.T) {
-		domains, err := NewDomains(sourceConfig{})
-		require.NoError(t, err)
-
-		require.Nil(t, domains.gitlab)
-		require.NotNil(t, domains.disk)
-	})
-
-	t.Run("when source is set to disk", func(t *testing.T) {
-		domains, err := NewDomains(sourceConfig{
-			domainSource: "disk",
+			require.Equal(t, tt.expectGitlabNil, domains.gitlab == nil)
+			require.NotNil(t, domains.disk)
 		})
-		require.NoError(t, err)
-
-		require.Nil(t, domains.gitlab)
-		require.NotNil(t, domains.disk)
-	})
+	}
 }
 
 func TestGetDomain(t *testing.T) {
-	gitlabSourceConfig.Domains.Enabled = []string{"new-source-test.gitlab.io"}
-	gitlabSourceConfig.Domains.Broken = "pages-broken-poc.gitlab.io"
-
-	t.Run("when requesting a test domain", func(t *testing.T) {
-		testDomain := gitlabSourceConfig.Domains.Enabled[0]
+	t.Run("when requesting an existing domain", func(t *testing.T) {
+		testDomain := "new-source-test.gitlab.io"
 
 		newSource := NewMockSource()
 		newSource.On("GetDomain", testDomain).
@@ -78,45 +97,27 @@ func TestGetDomain(t *testing.T) {
 		newSource.On("IsReady").Return(true).Once()
 		defer newSource.AssertExpectations(t)
 
-		domains := newTestDomains(t, newSource)
+		domains := newTestDomains(t, newSource, sourceGitlab)
 
-		domains.GetDomain(testDomain)
+		domain, err := domains.GetDomain(testDomain)
+		require.NoError(t, err)
+		require.NotNil(t, domain)
 	})
 
-	t.Run("when requesting a non-test domain", func(t *testing.T) {
+	t.Run("when requesting a domain that doesn't exist", func(t *testing.T) {
 		newSource := NewMockSource()
+		newSource.On("GetDomain", "does-not-exist.test.io").
+			Return(nil, nil).
+			Once()
 		newSource.On("IsReady").Return(true).Once()
 
 		defer newSource.AssertExpectations(t)
 
-		domains := newTestDomains(t, newSource)
+		domains := newTestDomains(t, newSource, sourceGitlab)
 
-		domain, err := domains.GetDomain("domain.test.io")
-
+		domain, err := domains.GetDomain("does-not-exist.test.io")
 		require.NoError(t, err)
 		require.Nil(t, domain)
-	})
-
-	t.Run("when requesting a broken test domain", func(t *testing.T) {
-		newSource := NewMockSource()
-		defer newSource.AssertExpectations(t)
-
-		domains := newTestDomains(t, newSource)
-
-		domain, err := domains.GetDomain("pages-broken-poc.gitlab.io")
-
-		require.Nil(t, domain)
-		require.EqualError(t, err, "broken test domain used")
-	})
-
-	t.Run("when requesting a test domain in case of the source not being fully configured", func(t *testing.T) {
-		domains, err := NewDomains(sourceConfig{})
-		require.NoError(t, err)
-
-		domain, err := domains.GetDomain("new-source-test.gitlab.io")
-
-		require.Nil(t, domain)
-		require.NoError(t, err)
 	})
 
 	t.Run("when requesting a serverless domain", func(t *testing.T) {
@@ -126,13 +127,14 @@ func TestGetDomain(t *testing.T) {
 		newSource.On("GetDomain", testDomain).
 			Return(&domain.Domain{Name: testDomain}, nil).
 			Once()
-		newSource.On("IsReady").Return(true).Once()
 
 		defer newSource.AssertExpectations(t)
 
-		domains := newTestDomains(t, newSource)
+		domains := newTestDomains(t, newSource, sourceGitlab)
 
-		domains.GetDomain(testDomain)
+		domain, err := domains.GetDomain(testDomain)
+		require.NoError(t, err)
+		require.NotNil(t, domain)
 	})
 }
 
@@ -150,78 +152,12 @@ func TestIsServerlessDomain(t *testing.T) {
 	})
 }
 
-func TestGetDomainWithIncrementalrolloutOfGitLabSource(t *testing.T) {
-	// This will produce the following pseudo-random sequence: 5, 87, 68
-	rand.Seed(42)
-
-	// Generates FNV hash 4091421005, 4091421005 % 100 = 5
-	domain05 := "test-domain-a.com"
-	// Generates FNV 2643293380, 2643293380 % 100 = 80
-	domain80 := "test-domain-b.com"
-
-	gitlabSourceConfig.Domains.Rollout.Percentage = 80
-
-	type testDomain struct {
-		name   string
-		source string
-	}
-
-	tests := map[string]struct {
-		stickiness string
-		domains    []testDomain
-	}{
-		// domain05 should always use gitlab source,
-		// domain80 should use disk source
-		"default stickiness": {
-			stickiness: "",
-			domains: []testDomain{
-				{name: domain05, source: "gitlab"},
-				{name: domain80, source: "disk"},
-				{name: domain05, source: "gitlab"},
-			},
-		},
-		// Given that randSeed(42) will produce the following pseudo-random sequence:
-		// {5, 87, 68} the first and third call for domain05 should use gitlab source,
-		// while the second one should use disk source
-		"no stickiness": {
-			stickiness: "random",
-			domains: []testDomain{
-				{name: domain05, source: "gitlab"},
-				{name: domain05, source: "disk"},
-				{name: domain05, source: "gitlab"},
-			}},
-	}
-
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
-			gitlabSource := NewMockSource()
-			for _, d := range tc.domains {
-				if d.source == "gitlab" {
-					gitlabSource.On("GetDomain", d.name).
-						Return(&domain.Domain{Name: d.name}, nil).
-						Once()
-				}
-			}
-			gitlabSource.On("IsReady").Return(true)
-			defer gitlabSource.AssertExpectations(t)
-
-			domains := newTestDomains(t, gitlabSource)
-
-			gitlabSourceConfig.Domains.Rollout.Stickiness = tc.stickiness
-
-			for _, domain := range tc.domains {
-				_, err := domains.GetDomain(domain.name)
-				require.NoError(t, err)
-			}
-		})
-	}
-}
-
-func newTestDomains(t *testing.T, gitlabSource *MockSource) *Domains {
+func newTestDomains(t *testing.T, gitlabSource *MockSource, config configSource) *Domains {
 	t.Helper()
 
 	return &Domains{
-		disk:   disk.New(),
-		gitlab: gitlabSource,
+		configSource: config,
+		gitlab:       gitlabSource,
+		disk:         disk.New(),
 	}
 }
