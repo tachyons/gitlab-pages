ca640f810a5ec21647df1ef17b1ec319405d0001
Jaime Martinez <jmartinez@gitlab.com>
2020-07-30 15:46:20 +1000

    Remove unused code

--- internal/source/gitlab/gitlab_poll.go
+++ internal/source/gitlab/gitlab_poll.go
@@ -20,15 +20,14 @@ func (g *Gitlab) poll(interval, maxElapsedTime time.Duration) {
 	backOff.InitialInterval = interval
 	backOff.MaxElapsedTime = maxElapsedTime
 
-	op := func() error {
+	operation := func() error {
 		log.Info("Checking GitLab internal API availability")
 
 		return g.client.Status()
 	}
 
-	err := backoff.Retry(op, backOff)
+	err := backoff.Retry(operation, backOff)
 	if err != nil {
-		// Handle error.
 		log.WithError(err).Errorf("Failed to connect to the internal GitLab API after %.2fs", interval.Seconds())
 		return
 	}
@@ -36,24 +35,6 @@ func (g *Gitlab) poll(interval, maxElapsedTime time.Duration) {
 	g.mu.Lock()
 	g.isReady = true
 	g.mu.Unlock()
+
 	log.Info("GitLab internal pages status API connected successfully")
-	//
-	// var err error
-	// for i := 0; i <= retries; i++ {
-	// 	log.Info("polling GitLab internal pages status API")
-	// 	err = g.checker.Status()
-	// 	if err == nil {
-	// 		log.Info("GitLab internal pages status API connected successfully")
-	// 		g.mu.Lock()
-	// 		g.isReady = true
-	// 		g.mu.Unlock()
-	//
-	// 		// return as soon as we connect to the API
-	// 		return
-	// 	}
-	//
-	// 	time.Sleep(interval)
-	// }
-	//
-	// log.WithError(err).Errorf("polling failed after %d tries every %.2fs", retries+1, interval.Seconds())
 }
