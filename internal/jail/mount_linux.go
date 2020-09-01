4e64869e248537b2bb8719c47620597595a46ea4
Kamil Trzciński <ayufan@ayufan.eu>
2020-09-01 12:18:10 +0200

    Unshare mount namespace to fix `getcwd`
    Previously we would call `LazyUnbind` as soon
    as proces starts. This does break `getcwd`
    as it starts returning `(unreachable)`
    due to mountpoint being gone from `chroot`.
    
    This removes the need for `LazyUnbind` and
    instead `unshare` mount namespace, making
    the new process to have it's own mounts
    that are not shared with parent process.
    The mounts gets cleaned-up once the process
    exists.

--- internal/jail/mount_linux.go
+++ internal/jail/mount_linux.go
@@ -2,9 +2,32 @@ package jail
 
 import (
 	"fmt"
+	"syscall"
 
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/sys/unix"
 )
+
+// Unshare makes the process to own it's own mount namespace
+// and prevent the changes for mounts to be propagated
+// to other mount namespace. Making all mount changes local
+// to current process
+func (j *Jail) Unshare() error {
+	// https://man7.org/linux/man-pages/man7/mount_namespaces.7.html
+	err := syscall.Unshare(syscall.CLONE_NEWNS)
+	log.WithError(err).Info("unsharing mount namespace")
+	if err != nil {
+		return err
+	}
+
+	// As documented in `mount_namespaces`:
+	// An application that creates a new mount namespace directly using
+	// clone(2) or unshare(2) may desire to prevent propagation of mount
+	// events to other mount namespaces
+	err = syscall.Mount("none", "/", "", unix.MS_REC|unix.MS_PRIVATE, "")
+	log.WithError(err).Info("changing root filesystem propagation")
+	return err
+}
 
 func (j *Jail) mount() error {
 	for dest, src := range j.bindMounts {
