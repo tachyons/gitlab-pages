80dc1ac5d70c77a89a535e331dabb614c873f100
Kamil Trzcinski <ayufan@ayufan.eu>
2020-09-08 11:47:21 +0200

    Fix `TestVFSRoot` and `TestReadlinkAbsolutePath`

--- internal/vfs/local/vfs_test.go
+++ internal/vfs/local/vfs_test.go
@@ -13,6 +13,19 @@ import (
 
 var localVFS = &VFS{}
 
+func tmpDir(t *testing.T) (string, func()) {
+	tmpDir, err := ioutil.TempDir("", "vfs")
+	require.NoError(t, err)
+
+	// On some systems `/tmp` can be a symlink
+	tmpDir, err = filepath.EvalSymlinks(tmpDir)
+	require.NoError(t, err)
+
+	return tmpDir, func() {
+		os.RemoveAll(tmpDir)
+	}
+}
+
 func TestVFSRoot(t *testing.T) {
 	// create structure as:
 	// /tmp/dir: directory
@@ -21,12 +34,11 @@ func TestVFSRoot(t *testing.T) {
 	// /tmp/file: file
 	// /tmp/file_link: symlink to `file`
 	// /tmp/file_absolute_link: symlink to `/tmp/file`
-	tmpDir, err := ioutil.TempDir("", "vfs")
-	require.NoError(t, err)
-	defer os.RemoveAll(tmpDir)
+	tmpDir, cleanup := tmpDir(t)
+	defer cleanup()
 
 	dirPath := filepath.Join(tmpDir, "dir")
-	err = os.Mkdir(dirPath, 0755)
+	err := os.Mkdir(dirPath, 0755)
 	require.NoError(t, err)
 
 	filePath := filepath.Join(tmpDir, "file")
