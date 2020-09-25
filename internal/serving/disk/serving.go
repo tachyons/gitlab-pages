a8bb79f6fa4513ac3078f5cea4c10862f190c91e
Florian Engelhardt <flo@dotbox.org>
2020-09-25 11:38:25 +0200

    PATCH-44819 Move addMimeType to main package

--- internal/serving/disk/serving.go
+++ internal/serving/disk/serving.go
@@ -1,10 +1,7 @@
 package disk
 
 import (
-	"mime"
 	"os"
-
-	"gitlab.com/gitlab-org/labkit/log"
 
 	"gitlab.com/gitlab-org/gitlab-pages/internal/httperrors"
 	"gitlab.com/gitlab-org/gitlab-pages/internal/serving"
@@ -46,24 +43,10 @@ func (s *Disk) ServeNotFoundHTTP(h serving.Handler) {
 // New returns a serving instance that is capable of reading files
 // from the VFS
 func New(vfs vfs.VFS) serving.Serving {
-	addExtraMIMETypes(extraMIMETypes)
-
 	return &Disk{
 		reader: Reader{
 			fileSizeMetric: metrics.DiskServingFileSize,
 			vfs:            vfs,
 		},
-	}
-}
-
-var extraMIMETypes = map[string]string{
-	".avif": "image/avif",
-}
-
-func addExtraMIMETypes(mimeTypes map[string]string) {
-	for ext, mimeType := range mimeTypes {
-		if err := mime.AddExtensionType(ext, mimeType); err != nil {
-			log.WithError(err).Errorf("failed to add extension: %q with MIME type: %q", ext, mimeType)
-		}
 	}
 }
