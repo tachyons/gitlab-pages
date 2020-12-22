875886462aceff1df4a15da95149734f9666a6d4
Vladimir Shushlin <vshushlin@gitlab.com>
2020-12-22 11:36:46 +0000

    Reject all unknown http methods

--- acceptance_test.go
+++ acceptance_test.go
@@ -2156,3 +2156,18 @@ func TestZipServing(t *testing.T) {
 		})
 	}
 }
+
+func TestUnknownHTTPMethod(t *testing.T) {
+	skipUnlessEnabled(t)
+	teardown := RunPagesProcess(t, *pagesBinary, listeners, "")
+	defer teardown()
+
+	req, err := http.NewRequest("UNKNOWN", listeners[0].URL(""), nil)
+	require.NoError(t, err)
+	req.Host = ""
+
+	resp, err := DoPagesRequest(t, req)
+	require.NoError(t, err)
+
+	require.Equal(t, http.StatusMethodNotAllowed, resp.StatusCode)
+}
---
+++ internal/rejectmethods/middleware.go
@@ -0,0 +1,30 @@
+package rejectmethods
+
+import (
+	"net/http"
+
+	"gitlab.com/gitlab-org/gitlab-pages/metrics"
+)
+
+var acceptedMethods = map[string]bool{
+	http.MethodGet:     true,
+	http.MethodHead:    true,
+	http.MethodPost:    true,
+	http.MethodPut:     true,
+	http.MethodPatch:   true,
+	http.MethodConnect: true,
+	http.MethodOptions: true,
+	http.MethodTrace:   true,
+}
+
+// NewMiddleware returns middleware which rejects all unknown http methods
+func NewMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if acceptedMethods[r.Method] {
+			handler.ServeHTTP(w, r)
+		} else {
+			metrics.RejectedRequestsCount.Inc()
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
+	})
+}
