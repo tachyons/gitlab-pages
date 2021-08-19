9ce45c3e795ebc956016df48008057d1b429542a
feistel <6742251-feistel@users.noreply.gitlab.com>
2021-08-19 06:01:17 +0000

    test: add back domains test
    this was actually a valid test, we just need to make sure the
    test case related to disk source are pruned and gitlab source
    config is being tested

--- internal/source/domains_test.go
+++ internal/source/domains_test.go
@@ -3,11 +3,65 @@ package source
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
+	"gitlab.com/gitlab-org/gitlab-pages/internal/config"
 	"gitlab.com/gitlab-org/gitlab-pages/internal/domain"
 )
+
+func TestNewDomains(t *testing.T) {
+	validCfg := config.GitLab{
+		InternalServer:     "https://gitlab.com",
+		APISecretKey:       []byte("abc"),
+		ClientHTTPTimeout:  time.Second,
+		JWTTokenExpiration: time.Second,
+	}
+
+	tests := []struct {
+		name        string
+		config      config.GitLab
+		expectedErr string
+	}{
+		{
+			name:   "gitlab_source_success",
+			config: validCfg,
+		},
+		{
+			name: "gitlab_source_no_url",
+			config: func() config.GitLab {
+				cfg := validCfg
+				cfg.InternalServer = ""
+
+				return cfg
+			}(),
+			expectedErr: "GitLab API URL or API secret has not been provided",
+		},
+		{
+			name: "gitlab_source_no_secret",
+			config: func() config.GitLab {
+				cfg := validCfg
+				cfg.APISecretKey = []byte{}
+
+				return cfg
+			}(),
+			expectedErr: "GitLab API URL or API secret has not been provided",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			domains, err := NewDomains(&tt.config)
+			if tt.expectedErr != "" {
+				require.EqualError(t, err, tt.expectedErr)
+				return
+			}
+			require.NoError(t, err)
+			require.NotNil(t, domains.gitlab)
+		})
+	}
+}
 
 func TestGetDomain(t *testing.T) {
 	t.Run("when requesting an existing domain for gitlab source", func(t *testing.T) {
