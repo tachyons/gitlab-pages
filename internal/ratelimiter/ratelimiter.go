128bf6b4cbee628cdb1fe3ee26b9442a18b85ef3
Jaime Martinez <jmartinez@gitlab.com>
2021-09-22 15:14:39 +1000

    test: use goroutines

--- internal/ratelimiter/ratelimiter.go
+++ internal/ratelimiter/ratelimiter.go
@@ -10,37 +10,34 @@ import (
 )
 
 const (
-	// DefaultPerDomainFrequency the maximum number of requests per second to be allowed per domain.
-	// The default value of 25ms equals 1 request every 25ms -> 40 rps
-	DefaultPerDomainFrequency = 25 * time.Millisecond
-	// DefaultPerDomainBurstSize is the maximum burst allowed per rate limiter
-	// E.g. The first 40 requests within 25ms will succeed, but the 41st will fail until the next
+	// DefaultPerDomainFrequency is the rate in time.Duration at which the rate.Limiter
+	// bucket is filled with 1 token. A token is equivalent to a request.
+	// The default value of 20ms, or 1 request every 20ms, equals 50 requests per second.
+	DefaultPerDomainFrequency = 20 * time.Millisecond
+	// DefaultPerDomainBurstSize is the maximum burst allowed per rate limiter.
+	// E.g. The first 50 requests within 20ms will succeed, but the 51st will fail until the next
 	// refill occurs at DefaultPerDomainFrequency, allowing only 1 request per rate frequency.
-	DefaultPerDomainBurstSize = 40
+	DefaultPerDomainBurstSize = 50
 
-	// avg of ~18,000 unique domains per hour
+	// based on an avg of ~18,000 unique domains per hour
 	// https://log.gprd.gitlab.net/app/lens#/edit/3c45a610-15c9-11ec-a012-eb2e5674cacf?_g=h@e78830b
 	defaultDomainsItems              = 20000
 	defaultDomainsExpirationInterval = time.Hour
 )
 
-type cache interface {
-	FindOrFetch(cacheNamespace, key string, fetchFn func() (interface{}, error)) (interface{}, error)
-}
-
 // Option function to configure a RateLimiter
 type Option func(*RateLimiter)
 
-// RateLimiter holds a map ot domain names with counters that enable rate limiting per domain.
+// RateLimiter holds an LRU cache of elements to be rate limited. Currently, it supports
+// a domainsCache and each item returns a rate.Limiter.
 // It uses "golang.org/x/time/rate" as its Token Bucket rate limiter per domain entry.
 // See example https://www.fatalerrors.org/a/design-and-implementation-of-time-rate-limiter-for-golang-standard-library.html
-// Cleanup runs every cleanupTimer iteration over all domains and removing them if
-// the time since counter.lastSeen is greater than the domainMaxTTL.
+// It also holds a now function that can be mocked in unit tests.
 type RateLimiter struct {
 	now                func() time.Time
 	perDomainFrequency time.Duration
 	perDomainBurstSize int
-	domainsCache       cache
+	domainsCache       *lru.Cache
 	// TODO: add sourceIPCache https://gitlab.com/gitlab-org/gitlab-pages/-/issues/630
 }
 
@@ -101,6 +98,6 @@ func (rl *RateLimiter) getDomainCounter(domain string) *rate.Limiter {
 func (rl *RateLimiter) DomainAllowed(domain string) (res bool) {
 	limiter := rl.getDomainCounter(domain)
 
-	// AllowN allows us to use the rl.now function so we can test this more easily.
+	// AllowN allows us to use the rl.now function, so we can test this more easily.
 	return limiter.AllowN(rl.now(), 1)
 }
