db4dd9522ba7e20713cab59664b85412d5a03cbb
Jaime Martinez <jmartinez@gitlab.com>
2021-09-22 17:33:40 +1000

    feat: add FF_ENABLE_RATE_LIMITER env
    (cherry picked from commit f67ad8042fd2939f417e83bbbad146daa6eaef3c)

--- internal/ratelimiter/middleware.go
+++ internal/ratelimiter/middleware.go
@@ -3,8 +3,12 @@ package ratelimiter
 import (
 	"net"
 	"net/http"
+	"os"
+
+	"github.com/sirupsen/logrus"
 
 	"gitlab.com/gitlab-org/gitlab-pages/internal/httperrors"
+	"gitlab.com/gitlab-org/gitlab-pages/internal/logging"
 )
 
 // DomainRateLimiter middleware ensures that the requested domain can be served by the current
@@ -15,8 +19,19 @@ func DomainRateLimiter(rl *RateLimiter) func(http.Handler) http.Handler {
 			host := getHost(r)
 
 			if !rl.DomainAllowed(host) {
-				httperrors.Serve429(w)
-				return
+				logging.LogRequest(r).WithFields(logrus.Fields{
+					"handler":                 "domain_rate_limiter",
+					"pages_domain":            host,
+					"rate_limiter_enabled":    rateLimiterEnabled(),
+					"rate_limiter_frequency":  rl.perDomainFrequency,
+					"rate_limiter_burst_size": rl.perDomainBurstSize,
+				}).Info("domain hit rate limit")
+
+				// Only drop requests once FF_ENABLE_RATE_LIMITER is enabled
+				if rateLimiterEnabled() {
+					httperrors.Serve429(w)
+					return
+				}
 			}
 
 			handler.ServeHTTP(w, r)
@@ -31,4 +46,8 @@ func getHost(r *http.Request) string {
 	}
 
 	return host
+}
+
+func rateLimiterEnabled() bool {
+	return os.Getenv("FF_ENABLE_RATE_LIMITER") == "true"
 }
