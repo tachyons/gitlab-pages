70ecebcc9cf140016e8330a9a00feee672f781f7
feistel <6742251-feistel@users.noreply.gitlab.com>
2021-10-01 14:06:46 +0200

    test: change import order in mock file

--- internal/mocks/mocks.go
+++ internal/mocks/mocks.go
@@ -5,9 +5,10 @@
 package mocks
 
 import (
-	gomock "github.com/golang/mock/gomock"
 	http "net/http"
 	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
 )
 
 // MockArtifact is a mock of Artifact interface
