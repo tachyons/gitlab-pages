4c1f49999b2950007455fa98d0a960693edb1f1f
Gerard Hickey <ghickey@gitlab.com>
2021-10-18 12:06:24 -0700

    Updated redhat_certification.rb per maintainer suggestions

--- build-scripts/redhat_certification.rb
+++ build-scripts/redhat_certification.rb
@@ -32,28 +32,24 @@ def is_regular_tag
 end
 
 def read_project_data
-  begin
-    JSON.parse(ENV['REDHAT_PROJECT_JSON'])
-  rescue => e
-    puts "Unable to parse JSON: #{e.message}"
-    puts e.backtrace
-    raise
-  end
+  JSON.parse(ENV['REDHAT_PROJECT_JSON'])
+rescue => e
+  puts "Unable to parse JSON: #{e.message}"
+  puts e.backtrace
+  raise
 end
 
 # return either the sha256 of the image or the symbol :no_skopeo
 def image_sha256(registry_spec)
-  begin
-    skopeo_out = %x(skopeo inspect docker://#{registry_spec} 2>/dev/null)
-    JSON.parse(skopeo_out)['Digest']
-  rescue Errno::ENOENT
-    return :no_skopeo
-  rescue JSON::ParserError
-    return :no_image
-  end
+  skopeo_out = %x(skopeo inspect docker://#{registry_spec} 2>/dev/null)
+  JSON.parse(skopeo_out)['Digest']
+rescue Errno::ENOENT
+  return :no_skopeo
+rescue JSON::ParserError
+  return :no_image
 end
 
-#
+# handle a call the Red Hat API and return the response object
 def redhat_api(method, endpoint, token=nil, payload=nil)
   uri = URI.parse(endpoint)
   http = Net::HTTP.new(uri.host, uri.port)
@@ -74,7 +70,7 @@ def redhat_api(method, endpoint, token=nil, payload=nil)
 
   begin
     resp = http.request(req)
-  rescue Exception => e
+  rescue StandardError => e
     puts "Unhandled exception for #{name}: #{e}"
     errors << "#{name}: Unhandled exception: #{e}"
   end
@@ -82,11 +78,10 @@ def redhat_api(method, endpoint, token=nil, payload=nil)
   return resp
 end
 
-
 def display_scan_status(token)
   results = redhat_api(:get,
               'https://catalog.redhat.com/api/containers/v1/projects/certifications/requests/scans',
-              token=token)
+              token)
 
   # invert the GitLab container name to Red Hat proj ID hash
   rh_pid = {}
@@ -95,7 +90,7 @@ def display_scan_status(token)
   puts " Status     Red Hat Project ID          Request ID                  GitLab Project:Tag"
   puts "=======    ========================    ========================    ==================================="
 
-  stats = {:pending => 0, :running => 0}
+  stats = {pending: 0, running: 0}
   JSON.parse(results.body)['data'].each do |scan_req|
     puts "#{scan_req['status']}    #{scan_req['cert_project']}    #{scan_req['_id']}    #{rh_pid[scan_req['cert_project']]}:#{scan_req['tag']}"
     case scan_req['status']
@@ -105,13 +100,11 @@ def display_scan_status(token)
       stats[:running] += 1
     end
   end
-  puts ""
-  puts "Total pending: #{stats[:pending]}\t\tTotal running: #{stats[:running]}"
+
+  puts "\nTotal pending: #{stats[:pending]}\t\tTotal running: #{stats[:running]}"
 end
 
-
-token = ENV.keys.include?('REDHAT_API_TOKEN') ? ENV['REDHAT_API_TOKEN'] : nil
-options = {:status => false, :token => token }
+options = {status: false, token: ENV['REDHAT_API_TOKEN'] }
 optparse = OptionParser.new do |opts|
   opts.banner = "Usage: #{File.basename $0} [options] "
 
@@ -136,7 +129,7 @@ end
 
 # Remove CE/EE suffix and add `-ubi8` to the commit ref if not already present.
 version = ARGV[0].sub(/-(ce|ee)$/, '')
-if not version.end_with? '-ubi8'
+unless version.end_with? '-ubi8'
   # we add `-ubi8` to find the UBI images.
   version += '-ubi8'
 end
@@ -172,9 +165,8 @@ $IMAGE_VERSION_VAR.keys.each do |name|
                 'tag'       => version }
     resp = redhat_api(:post, endpoint, token=token, payload=payload)
 
-
     puts "API call for #{name} returned #{resp.code}: #{resp.message}"
-    if resp.code.to_i < 200 or resp.code.to_i > 299
+    if resp.code.to_i < 200 || resp.code.to_i > 299
       errors << "API call for #{name} returned #{resp.code}: #{resp.message}"
     end
   else
@@ -187,8 +179,8 @@ end
 # display the collected errors in the CI job output
 unless errors.empty?
   puts "\n\nThe following errors have been collected:"
-  errors.each { |err|
+  errors.each do |err|
     puts "\t- #{err}"
-  }
+  end
   exit(1)
 end
