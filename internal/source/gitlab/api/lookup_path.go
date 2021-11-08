87788a7d6f1b6a5d85a633939da71b3957eed5f3
feistel <6742251-feistel@users.noreply.gitlab.com>
2021-11-08 16:16:04 +0100

    refactor: add file_count and file_size to lookup_path source

--- internal/source/gitlab/api/lookup_path.go
+++ internal/source/gitlab/api/lookup_path.go
@@ -14,4 +14,6 @@ type Source struct {
 	Type   string `json:"type,omitempty"`
 	Path   string `json:"path,omitempty"`
 	SHA256 string `json:"sha256,omitempty"`
+	Count  int    `json:"file_count,omitempty"`
+	Size   int    `json:"file_size,omitempty"`
 }
