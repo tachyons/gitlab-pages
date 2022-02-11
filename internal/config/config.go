fc5a652574d0eef03c776a70f3c0678158bc1dbd
Kassio Borges <kborges@gitlab.com>
2022-02-11 14:31:56 -0300

    Fix weak HTTP server timeouts configuration
    The timeouts are now configurable by the flags:
    - server-read-timeout
    - server-read-header-timeout
    - server-write-timeout
    - server-keep-alive
    
    Related to: https://gitlab.com/gitlab-org/gitlab-pages/-/issues/684
    
    Changelog: security

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,6 +20,7 @@ type Config struct {
 	GitLab          GitLab
 	Log             Log
 	Sentry          Sentry
+	Server          Server
 	TLS             TLS
 	Zip             ZipServing
 
@@ -137,6 +138,13 @@ type ZipServing struct {
 	AllowedPaths       []string
 }
 
+type Server struct {
+	ReadTimeout       time.Duration
+	ReadHeaderTimeout time.Duration
+	WriteTimeout      time.Duration
+	ListenKeepAlive   time.Duration
+}
+
 func internalGitlabServerFromFlags() string {
 	if *internalGitLabServer != "" {
 		return *internalGitLabServer
@@ -240,6 +248,12 @@ func loadConfig() (*Config, error) {
 			RefreshInterval:    *zipCacheRefresh,
 			OpenTimeout:        *zipOpenTimeout,
 			AllowedPaths:       []string{*pagesRoot},
+		},
+		Server: Server{
+			ReadTimeout:       *serverReadTimeout,
+			ReadHeaderTimeout: *serverReadHeaderTimeout,
+			WriteTimeout:      *serverWriteTimeout,
+			ListenKeepAlive:   *serverKeepAlive,
 		},
 
 		// Actual listener pointers will be populated in appMain. We populate the
